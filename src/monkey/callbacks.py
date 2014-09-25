3714a101031ae98a9420effd297a1c8ef856fa35
Sean Purdon <sean.purdon@gmail.com>
2014-09-25 15:22:24 +1000

    Added event tree parsing.
    This is currently *very* slow

--- src/monkey/callbacks.py
+++ src/monkey/callbacks.py
@@ -2,15 +2,53 @@ import inspect
 import sys
 
 
+class EventManager(object):
+    def __init__(self):
+        self._events = []
+
+        self._topLevelEvents = []  # at top of call stack; child of nothing
+
+    def _findParent(self, frames, obj=None):
+        events = obj.children if obj is not None else self._topLevelEvents
+
+        for evt in events:
+            if evt.frame in frames:
+                parent = self._findParent(frames, evt)
+
+                return parent if parent is not None else evt
+
+    def addEvent(self, event):
+        # try to find a parent
+        # this will be the LOWEST (ie, earliest) item on the stack
+        frames = [frame for frame,_,_,_,_,_ in inspect.stack()]
+        parent = self._findParent(frames)
+
+        if parent:
+            parent.addChild(event)
+        else:
+            self._topLevelEvents.append(event)
+
+        # add it to the list of events
+        self._events.append(event)
+
+
+# Mimic a singleton with a module var
+# NB: This is not threadsafe (but neither is Tkinter)
+EVENT_MANAGER = EventManager()
+
+
 class Event(object):
-    def __init__(self, obj, fn, args, kwargs, result):
+    def __init__(self, obj, fn, args, kwargs):
         self.obj = obj
         self.fn = fn
 
         self.args = args
         self.kwargs = kwargs
 
-        self.result = result
+        self.result = None
+        self.frame = None
+
+        self._children = []
 
     def __str__(self):
         if self.obj is None:
@@ -19,19 +57,35 @@ class Event(object):
         return '{!r}.{}(...) -> {}'.format(self.obj, self.fn.__name__,
                 self.result)
 
+    @property
+    def children(self):
+        return self._children
+
+    def addChild(self, child):
+        self._children.append(child)
+
+    def __iter__(self):
+        for child in self._children:
+            yield child
+
 
 def log(fn, isMethod=True):
     def f(*args, **kwargs):
+        if isMethod:
+            evt = Event(args[0], fn, args[1:], kwargs)
+        else:
+            evt = Event(None, fn, args, kwargs)
+
+        # set the current frame as the event's frame
+        # this isn't entirely accurate (as the event represents the function
+        # we're about to call), but will work for our purposes
+        evt.frame = inspect.currentframe()
+
+        EVENT_MANAGER.addEvent(evt)
+
         result = fn(*args, **kwargs)
 
-        if isMethod:
-            self = args[0]
-            args = args[1:]
-        else:
-            self = None
-
-        evt = Event(self, fn, args, kwargs, result)
-        print evt
+        evt.result = result
 
         return result
     return f
